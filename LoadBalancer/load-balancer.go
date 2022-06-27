c46de6ffe6a36022a113eaaf7bc1431292b5d115
shaorui0 <sr1054461216@163.com>
2022-06-27 13:03:10 +0800

    update load-balancer.go

--- LoadBalancer/load-balancer.go
+++ LoadBalancer/load-balancer.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"context"
-	"flag"
+	// "flag"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"net/http/httptest"
 	"net/http/httputil"
 	"net/url"
 	"strings"
@@ -14,44 +15,40 @@ import (
 	"sync/atomic"
 	"time"
 )
+
 // backend struct define
-type Backend struct{
-	URL 	*url.URL
-	Alive 	bool
-	mux 	sync.RWMutex
-	ReverseProxy	*httputil.ReverseProxy
+type Backend struct {
+	URL          *url.URL
+	Alive        bool
+	mux          sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
 }
-// ReverseProxy is an HTTP Handler that takes an incoming request 
-// and sends it to another server
-// proxying the response back to the client.
 
 // store and track backend
-type ServerPool struct{
-	backends	[]*Backend
-	current 	uint64
-} 
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
 
-u, _ := url.Parse("http://localhost:8080")
-rp := httputil.NewSingleHostReverseProxy(u)
-http.HandlerFunc(rp.ServeHTTP)
-// initialize a reverse proxy  
-// relay requests to the passed url(localhost:8080)
-// all response sent to client from here
+func (s *ServerPool) AddBackend(backend *Backend) {
+	s.backends = append(s.backends, backend)
+}
 
 // selection process
 // avoid multiple clients request next peer->add mutex
 // Traverse the slice as a cycle, get next server index
 func (s *ServerPool) NextIndex() int {
-	return int(atomic.AddUnit64(&s.current, uint64(1)) % uint64(len(s.backends)))
+	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
 }
+
 // skip dead backends during the next pick
 // treverse find next alive backend
-func (s *ServerPool) GetNextPeer() *Backend{
+func (s *ServerPool) GetNextPeer() *Backend {
 	next := s.NextIndex()
 	l := len(s.backends) + next
-	for i := next; i < l; i++{
+	for i := next; i < l; i++ {
 		idx := i % len(s.backends)
-		if s.backends[idx].IsAlive(){
+		if s.backends[idx].IsAlive() {
 			if i != next {
 				atomic.StoreUint64(&s.current, uint64(idx))
 			}
@@ -61,21 +58,48 @@ func (s *ServerPool) GetNextPeer() *Backend{
 	return nil
 }
 
-func (b *Backend) SetAlive() (alive bool){
+func (b *Backend) SetAlive(alive bool) {
 	b.mux.Lock()
 	b.Alive = alive
 	b.mux.Unlock()
+
 }
 
-func (b *Backend) IsAlive() (alive bool){
+func (b *Backend) IsAlive() (alive bool) {
 	b.mux.RLock()
 	alive = b.Alive
 	b.mux.RUnlock()
 	return
 }
 
+// MarkBackendStatus changes a status of a backend
+func (s *ServerPool) MarkBackendStatus(backendUrl *url.URL, alive bool) {
+	for _, b := range s.backends {
+		if b.URL.String() == backendUrl.String() {
+			b.SetAlive(alive)
+			break
+		}
+	}
+}
+
+// GetAttemptsFromContext returns the attempts for request
+func GetAttemptsFromContext(r *http.Request) int {
+	if attempts, ok := r.Context().Value(Attempts).(int); ok {
+		return attempts
+	}
+	return 1
+}
+
+// GetAttemptsFromContext returns the attempts for request
+func GetRetryFromContext(r *http.Request) int {
+	if retry, ok := r.Context().Value(Retry).(int); ok {
+		return retry
+	}
+	return 0
+}
+
 // load balance request
-func lb(w http.ReverseWriter, r *http.Request){
+func lb(w http.ResponseWriter, r *http.Request) {
 	attempts := GetAttemptsFromContext(r)
 	if attempts > 3 {
 		log.Printf("%s(%s) Max attemps reached, terminating\n", r.RemoteAddr, r.URL.Path)
@@ -84,66 +108,31 @@ func lb(w http.ReverseWriter, r *http.Request){
 	}
 
 	peer := serverPool.GetNextPeer()
-	if peer != nil{
+	if peer != nil {
 		peer.ReverseProxy.ServeHTTP(w, r)
-		return 
+		return
 	}
 	http.Error(w, "Service not available", http.StatusServiceUnavailable)
-}
-
-server := http.Server{
-	Addr:    fmt.Sprintf(":%d", port),
-	Handler: http.HandlerFunc(lb),
 }
 
 // route traffic only to healthy backends, two ways:
 // active way:while current request coming, select a backend if it's unresponsive, mark it as down
 // passive way: ping backends on fixed intervals and check status
 
-// for active check way:
-proxy.ErrorHandler = func(writer http.ResponseWriter, request *http.Request, e error) {
-	log.Printf("[%s] %s\n", serverUrl.Host, e.Error())
-	retries := GetRetryFromContext(request)
-	if retries < 3 {
-	  select {
-		case <-time.After(10 * time.Millisecond):
-		  ctx := context.WithValue(request.Context(), Retry, retries+1)
-		  proxy.ServeHTTP(writer, request.WithContext(ctx))
-		}
-		return
-	  }
-  
-	// after 3 retries, mark this backend as down
-	serverPool.MarkBackendStatus(serverUrl, false)
-  
-	// if the same request routing for few attempts with different backends, increase the count
-	attempts := GetAttemptsFromContext(request)
-	log.Printf("%s(%s) Attempting retry %d\n", request.RemoteAddr, request.URL.Path, attempts)
-	ctx := context.WithValue(request.Context(), Attempts, attempts+1)
-	lb(writer, request.WithContext(ctx))
-  }
-
-  const (
-	  Attempts 	int = iota
-	  Retry
-  )
-
-  func GetRetryFromContext(r *http.Request) int {
-	  if retry, ok := r.Context().Value(Retry).(int); ok{
-		  return retry
-	  }
-	  return 0
-  }
+const (
+	Attempts int = iota
+	Retry
+)
 
 // passive check way:
-func  isBackendAlive(u *url.URL) bool {
+func isBackendAlive(u *url.URL) bool {
 	timeout := 2 * time.Second
 	conn, err := net.DialTimeout("tcp", u.Host, timeout)
-	if err != nil{
+	if err != nil {
 		log.Println("Site unreachable, error: ", err)
 		return false
 	}
-	_ = conn.Close()
+	defer conn.Close()
 	return true
 }
 
@@ -160,7 +149,7 @@ func (s *ServerPool) HealthCheck() {
 	}
 }
 
-// healthCheck runs a routine for check status of the backends 
+// healthCheck runs a routine for check status of the backends
 // every 20 secs
 func healthCheck() {
 	t := time.NewTicker(time.Second * 20)
@@ -168,19 +157,27 @@ func healthCheck() {
 		select {
 		case <-t.C:
 			log.Println("Starting health check...")
-      		serverPool.HealthCheck()
-      		log.Println("Health check completed")
+			serverPool.HealthCheck()
+			log.Println("Health check completed")
 		}
 	}
 }
 
 var serverPool ServerPool
+
 func main() {
-	var serverList string
 	var port int
-	flag.StringVar(&serverList, "backends", "", "Load balanced backends, use commas to separate")
-	flag.IntVar(&port, "port", 3030, "Port to serve")
-	flag.Parse()
+	backendServer1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "this call was relayed by the reverse proxy1")
+	}))
+	backendServer2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "this call was relayed by the reverse proxy2")
+	}))
+	backendServer3 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "this call was relayed by the reverse proxy3")
+	}))
+
+	serverList := fmt.Sprintf("%s,%s,%s", backendServer1.URL, backendServer2.URL, backendServer3.URL)
 
 	if len(serverList) == 0 {
 		log.Fatal("Please provide one or more backends to load balance")
---
+++ LoadBalancer/unit_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+
+	// "flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
+	"testing"
+)
+
+func TestAddBackend(t *testing.T) {
+	backendServer1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "This is a backend ")
+	}))
+	defer backendServer1.Close()
+	URL, err := url.Parse(backendServer1.URL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var serverPool ServerPool
+	serverPool.AddBackend(&Backend{
+		URL:          URL,
+		Alive:        true,
+		ReverseProxy: httputil.NewSingleHostReverseProxy(URL),
+	})
+	if len(serverPool.backends) != 1 {
+		t.Errorf("AddBackend() error!")
+	}
+}
+
+func TestNextIndex(t *testing.T) {
+	var serverPool ServerPool
+	var backend1 Backend
+	var backend2 Backend
+	var backend3 Backend
+	serverPool.AddBackend(&backend1)
+	serverPool.AddBackend(&backend2)
+	serverPool.AddBackend(&backend3)
+	fmt.Printf(">>> len(serverPool): %d", len(serverPool.backends))
+
+	serverPool.NextIndex()
+	if serverPool.current != 1 {
+		t.Errorf("1:%d", serverPool.current)
+	}
+
+	serverPool.NextIndex()
+	if serverPool.current != 2 {
+		t.Errorf("2:%d", serverPool.current)
+	}
+
+	serverPool.NextIndex()
+	if serverPool.current != 3 {
+		t.Errorf("3:%d", serverPool.current)
+	}
+}
+
+// func TestGetNextPeer(t *testing.T) {
+// 	var serverPool ServerPool
+
+// }
